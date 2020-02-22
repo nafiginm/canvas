db57e5ddb4e4d46d7956b6130f485a02652b893c
nafiginm <nafiginm@yandex.ru>
2020-02-22 22:06:21 +0300

    changing calculation parameters

--- js/index.js
+++ js/index.js
@@ -1,101 +1,46 @@
-window.onload = draw();
-
-function draw() {
-  const canvas = document.querySelector('canvas');
+function drawStar(cx, cy, spikes, outerRadius, innerRadius) {
+  const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
-  let color =  ['red','blue','green','black','yellow'];
+  const color =  ['red','blue','green','yellow','black'];
 
-  for(let i = 0,j = 0; i < color.length,j < 5; i++, j++) {
-    let colorStar = color[i];
-    ctx.save();
-    ctx.translate(100 - Math.floor(Math.random() * 100),
-                  100 - Math.floor(Math.random() * 100));
+  for(let i = 0; i < 5; i++) {
+    const colorStar = color[i];
+    const step = Math.PI / spikes;
+    let rotation = Math.PI / 2 * 3;
+    let x = cx;
+    let y = cy;
+
+    ctx.translate(Math.floor(Math.random()*(100)), Math.floor(Math.random()*(100)));
     ctx.beginPath();
     ctx.save();
-    ctx.moveTo(75,250);
-    ctx.lineTo(150,50);
-    ctx.lineTo(225,250);
-    ctx.lineTo(50,120);
-    ctx.lineTo(250,120);
-    ctx.lineTo(75,250);
+
+    for (k = 0; k < spikes; k++) {
+      x = cx + Math.cos(rotation) * outerRadius;
+      y = cy + Math.sin(rotation) * outerRadius;
+      ctx.lineTo(x, y);
+      rotation += step;
+      x = cx + Math.cos(rotation) * innerRadius;
+      y = cy + Math.sin(rotation) * innerRadius;
+      ctx.lineTo(x, y);
+      rotation += step;
+    }
+
+    ctx.moveTo(cx, cy - outerRadius);
+    ctx.lineTo(cx, cy - outerRadius);
+    ctx.closePath();
     ctx.fillStyle = colorStar;
     ctx.fill();
     ctx.restore();
   }
-
   
-
-  // ctx.beginPath();
-  // ctx.save();
-  // ctx.moveTo(75,250);
-  // ctx.lineTo(150,50);
-  // ctx.lineTo(225,250);
-  // ctx.lineTo(50,120);
-  // ctx.lineTo(250,120);
-  // ctx.lineTo(75,250);
-  // ctx.fillStyle = 'red';
-  // ctx.fill();
-  // ctx.restore();
-  
-  // ctx.beginPath();
-  // ctx.save();
-  // ctx.translate(300,0);
-  // ctx.moveTo(75,250);
-  // ctx.lineTo(150,50);
-  // ctx.lineTo(225,250);
-  // ctx.lineTo(50,120);
-  // ctx.lineTo(250,120);
-  // ctx.lineTo(75,250);
-  // ctx.fillStyle = 'blue';
-  // ctx.fill();
-  // ctx.restore();
-
-  // ctx.beginPath();
-  // ctx.save();
-  // ctx.translate(0,300);
-  // ctx.moveTo(75,250);
-  // ctx.lineTo(150,50);
-  // ctx.lineTo(225,250);
-  // ctx.lineTo(50,120);
-  // ctx.lineTo(250,120);
-  // ctx.lineTo(75,250);
-  // ctx.fillStyle = 'black';
-  // ctx.fill();
-  // ctx.restore();
-
-  // ctx.beginPath();
-  // ctx.save();
-  // ctx.translate(300,300);
-  // ctx.moveTo(75,250);
-  // ctx.lineTo(150,50);
-  // ctx.lineTo(225,250);
-  // ctx.lineTo(50,120);
-  // ctx.lineTo(250,120);
-  // ctx.lineTo(75,250);
-  // ctx.fillStyle = 'yellow';
-  // ctx.fill();
-  // ctx.restore();
-
-  // ctx.beginPath();
-  // ctx.save();
-  // ctx.translate(150,150);
-  // ctx.moveTo(75,250);
-  // ctx.lineTo(150,50);
-  // ctx.lineTo(225,250);
-  // ctx.lineTo(50,120);
-  // ctx.lineTo(250,120);
-  // ctx.lineTo(75,250);
-  // ctx.fillStyle = 'green';
-  // ctx.fill();
-  // ctx.restore();
-
   canvas.addEventListener('click', (e) => {
     const x = e.offsetX;
     const y = e.offsetY;
     const img_data = ctx.getImageData(x, y, 1, 1);
-    const pix = img_data.data;
-  
+    const pix = img_data.data;  
     const smallCanvas = document.getElementById('small-canvas');
     smallCanvas.style.backgroundColor = `rgba(${pix.join(',')})`;
   });
 };
+
+drawStar(100, 100, 5, 100, 50);
